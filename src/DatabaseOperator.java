09e62159d072bfc97fa5761f8e791e25449d8a4f
Alloba <alexlbates@gmail.com>
2014-03-30 14:38:31 -0400

    Untested Finish of DatabaseOperator class
    i finished up most of the code from the databaseOperator class, although
    absolutely zero of it is tested.

--- src/DatabaseOperator.java
+++ src/DatabaseOperator.java
@@ -1,8 +1,10 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -15,18 +17,17 @@ import java.util.ArrayList;
  *
  * @author alexl_000
  */
-import java.nio.file.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import sun.dc.pr.PRException;
+import java.util.Random;
 
 public class DatabaseOperator {
 
-    public ArrayList<String> wordArray = new ArrayList<String>();
+    public ArrayList<String> wordArray = new ArrayList<>();
+    String fileLocation;
 
     public DatabaseOperator(String FileLocation) throws IOException {
         //Upon creation, load up the given text file and enter in all the words to an array.
 
+        this.fileLocation = FileLocation;
         BufferedReader fileRead = null;
 
         try {
@@ -43,6 +44,23 @@ public class DatabaseOperator {
             wordArray.add(endFileChecker);
             endFileChecker = fileRead.readLine();
         }
+        fileRead.close();
+    }
+
+    public void Save() throws IOException {
+        //save the arrayList to the file specified at class instantiation
+        File file = new File(this.fileLocation);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+
+        try (BufferedWriter bw = new BufferedWriter(fw)) {
+            for (String x : this.wordArray) {
+                bw.write(x);
+            }
+        }
     }
 
     public void AlpabetizeList(ArrayList<String> WordList) {
@@ -50,7 +68,36 @@ public class DatabaseOperator {
     }
 
     public void AddWords(ArrayList<String> wordList) {
-        this.wordArray.addAll(wordList);
+        //add words from the list, if they do not already exist in the list
+        //then sort the list.
+
+        for (String x : wordList) {
+            if (!this.wordArray.contains(x)) {
+                this.wordArray.add(x);
+            }
+        }
         AlpabetizeList(this.wordArray);
+    }
+
+    public String SelectWord(String weekDay) {
+        //Take the first letter of the given string, and create a sublist of words starting with that letter.
+
+        String workingLetter = weekDay.substring(0, 0).toLowerCase();
+        ArrayList<String> workingList = new ArrayList<>();
+
+        workingList.addAll(this.wordArray);
+
+        for (String x : workingList) {
+            //check each item in word list. if it isnt starting with the right letter, junk it.
+            if (!x.toLowerCase().startsWith(workingLetter)) {
+                workingList.remove(x);
+            }
+        }
+
+        int endIndex = workingList.size() - 1;
+        //randomly get one of the elements in the list and return it for use.
+        Random r;
+        r = new Random();
+        return workingList.get(r.nextInt(endIndex));
     }
 }
