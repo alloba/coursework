92107afe0fb1ec39095e3992feec8685276cd99b
Alex <alexlbates@gmail.com>
2016-02-23 12:27:35 -0500

    Initial commit

---
+++ lolwut.js
@@ -0,0 +1,30 @@
+/**
+ * Created by alexl_000 on 2/22/2016.
+ */
+/**
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    $('.menu-pane').css({
+        position:'absolute',
+        top:'0px',
+        left:'-20px',
+        width:'200px',
+        height:'100%',
+        background:'red',
+        fontSize:'30px'
+    }).show();
+
+    $('.top-title').css({
+        background:'blue',
+        fonsSize:'40px',
+        position:'absolute',
+        left:'200px',
+        height:'100%'
+    }).show();
+
+    $('.search-bar').css({
+
+    })
+});
+ **/
