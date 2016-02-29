8bb5a7f1f500cfc0ee732919139e9577ed36ac1f
Alex <alexlbates@gmail.com>
2016-02-29 16:57:29 -0500

    added a very proof of concept implementation of searching for champions. All it does is display the name and the blurb for the champion, but it at least can be an example moving forward.
    also removed some old junk

--- lolwut.js
+++ lolwut.js
@@ -1,45 +1,66 @@
 /**
  * Created by alexl_000 on 2/22/2016.
  */
-/**
-$(document).ready(function(){
 
-    // jQuery methods go here...
-    $('.menu-pane').css({
-        position:'absolute',
-        top:'0px',
-        left:'-20px',
-        width:'200px',
-        height:'100%',
-        background:'red',
-        fontSize:'30px'
-    }).show();
-
-    $('.top-title').css({
-        background:'blue',
-        fonsSize:'40px',
-        position:'absolute',
-        left:'200px',
-        height:'100%'
-    }).show();
-
-    $('.search-bar').css({
-
-    })
-});
- **/
 //$(document).ready(function(){
-//    $('.menu-pane').sideb
-//}
+//    $.getJSON('champion.json', function(data){
+//        console.log(data);
+//        var result = eval(data);
+//    });
+//
+//    for(var key in result){
+//        console.log(key);
+//    }
+//});
+//
+//$.getJSON('champion.json', function(response){
+//    console.log(response.data.Aatrox.key);
+//    console.log(Object.keys(response.data));
+//
+//    var champNameList = Object.keys(response.data);
+//    //$.each(keys, function(index, value){
+//    //    console.log(index + ':' + value);
+//    //});
+//    console.log(response.data['Aatrox'].key);
+//
+//    console.log("");
+//    return response;
+//});
 
-$.getJSON("champion.json", function(data){
-    console.log(data[0]);
-    var items = [];
-    $.each(data, function(i, field){
-        items.push(field);
+
+function findchampkey(name){
+    $.getJSON('champion.json', function(response){
+        //console.log(Object.keys(response.data));
+        console.log = response.data[name].key;
     });
-    console.log(items[0]);
-});
 
-//var champ_json = JSON.parse("/champion.json")['data'];
-//document.getElementsByClassName("champ-information-pane").innerHTML=champ_json;
+
+}
+
+function getChamp(name){
+    var val;
+    $.ajaxSetup({"async":false});
+    $.getJSON('champion.json', function(response){
+        try{val = response.data[name];}
+        catch(err){console.log('no champ found');}
+    });
+    return val;
+}
+
+function search(ele){
+    if(event.keyCode == 13){
+        var value = ele.value.charAt(0).toUpperCase() + ele.value.slice(1);
+        try{
+            console.log(getChamp(value).id);
+            $('.champ-information-pane').fadeOut(800, function(){
+                $('.champ-information-pane p').text(getChamp(value).id);
+                $('<p>' + getChamp(value).blurb + '<p>').appendTo('.champ-information-pane p');
+
+            });
+            $('.champ-information-pane').fadeIn(400, function(){});
+        }
+        catch (e){
+            console.log('champ name not found');
+        }
+    }
+}
