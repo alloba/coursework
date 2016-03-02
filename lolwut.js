b4c2f89bd89688b52302752ed124ff55767cd2c2
Alex <alexlbates@gmail.com>
2016-03-01 22:23:26 -0500

    changed the page quite a bit internally, added image loading to champ screens made stats load and go into the correct html element when the champ name is entered. lots of moving things around on the screen to make it look less broken.

--- lolwut.js
+++ lolwut.js
@@ -2,31 +2,9 @@
  * Created by alexl_000 on 2/22/2016.
  */
 
-//$(document).ready(function(){
-//    $.getJSON('champion.json', function(data){
-//        console.log(data);
-//        var result = eval(data);
-//    });
-//
-//    for(var key in result){
-//        console.log(key);
-//    }
-//});
-//
-//$.getJSON('champion.json', function(response){
-//    console.log(response.data.Aatrox.key);
-//    console.log(Object.keys(response.data));
-//
-//    var champNameList = Object.keys(response.data);
-//    //$.each(keys, function(index, value){
-//    //    console.log(index + ':' + value);
-//    //});
-//    console.log(response.data['Aatrox'].key);
-//
-//    console.log("");
-//    return response;
-//});
+$(document).ready(function(){
 
+});
 
 function findchampkey(name){
     $.getJSON('champion.json', function(response){
@@ -49,18 +27,70 @@ function getChamp(name){
 
 function search(ele){
     if(event.keyCode == 13){
-        var value = ele.value.charAt(0).toUpperCase() + ele.value.slice(1);
-        try{
-            console.log(getChamp(value).id);
-            $('.champ-information-pane').fadeOut(800, function(){
-                $('.champ-information-pane p').text(getChamp(value).id);
-                $('<p>' + getChamp(value).blurb + '<p>').appendTo('.champ-information-pane p');
+        var givenID = ele.value.charAt(0).toUpperCase() + ele.value.slice(1);
+        try {
+
+            console.log(getChamp(givenID).id);
+            $('.champ-information-pane').fadeOut(400, function(){
+                $(':hidden').show(1000);
+                gatherInfo(givenID);
+                $('.champ-information-pane').fadeIn(400, function(){});
 
             });
-            $('.champ-information-pane').fadeIn(400, function(){});
         }
         catch (e){
             console.log('champ name not found');
         }
     }
+}
+function gatherInfo(val){
+    $('.champ-information-pane .name').text(getChamp(val).id);
+    $('.champ-image').attr('src', 'Champion_Images/' + val + '.jpg');
+    /*
+    STARTING class .starting-stats
+    hp id           hp
+    armor id        armor
+    hpregen         hpregen
+    attackdamage    attackdamage
+    mana            mp
+    spell block     spellblock
+    manaregen       mpregen
+    attackspd       attackspeedoffset
+    movement        movespeed
+    attkrange       attackrange
+    crit            crit
+
+    PERLEVEL
+    hp              hpperlevel
+    spellblock      spellblockperlevel
+    crit            critperlevel
+    mana            mpperlevel
+    hpregen         hpregenperlevel
+    attkdmg         attackdamageperlevel
+    armor           armorperlevel
+    manaregen       mpregenperlevel
+    attkspd         attackspeedperlevel
+     */
+    $('#hp').text(getChamp(val).stats.hp);
+    $('#armor').text(getChamp(val).stats.armor);
+    $('#hpregen').text(getChamp(val).stats.hpregen);
+    $('#attackdamage').text(getChamp(val).stats.attackdamage);
+    $('#mp').text(getChamp(val).stats.mp);
+    $('#spellblock').text(getChamp(val).stats.spellblock);
+    $('#mpregen').text(getChamp(val).stats.mpregen);
+    $('#attackspeedoffset').text(getChamp(val).stats.attackspeedoffset);
+    $('#movespeed').text(getChamp(val).stats.movespeed);
+    $('#attackrange').text(getChamp(val).stats.attackrange);
+    $('#crit').text(getChamp(val).stats.crit);
+
+    $('#hpperleve').text(getChamp(val).stats.hpperlevel);
+    $('#spellblockperlevel').text(getChamp(val).stats.spellblockperlevel);
+    $('#critperlevel').text(getChamp(val).stats.critperlevel);
+    $('#mpperlevel').text(getChamp(val).stats.mpperlevel);
+    $('#hpregenperlevel').text(getChamp(val).stats.hpregenperlevel);
+    $('#attackdamageperlevel').text(getChamp(val).stats.attackdamageperlevel);
+    $('#armorperlevel').text(getChamp(val).stats.armorperlevel);
+    $('#mpregenperlevel').text(getChamp(val).stats.mpregenperlevel);
+    $('#attackspeedperlevel').text(getChamp(val).stats.attackspeedperlevel);
+
 }
