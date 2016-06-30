3f7be04b5886b81c950a2dd40e08e7555aaa474e
alex.bates <alexlbates@gmail.com>
2016-06-30 15:28:21 -0400

    got everything kind of working. image saves correctly, i convert the image into the original dataset format correctly. the actual number guessing is pretty bad still though. much below what it should be

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ import tkinter.filedialog
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageChops
+from PIL import ImageOps
 import pickle
 
 from Network import Network
@@ -18,7 +19,7 @@ class TKApp:
 
         self.window.configure(menu=self.menubar)
 
-        self.canvas_height = 800
+        self.canvas_height = 600
         self.canvas_width = 600
         self.canvas_center = self.canvas_height//2
 
@@ -44,10 +45,10 @@ class TKApp:
             self.point_list.append(self.point_list[0])
             self.point_list.append(self.point_list[1])
 
-        self.canvas.create_line(self.point_list, tags='theline', width=20)
-        self.canvas.create_oval(event.x-10, event.y-10, event.x+10, event.y+10, tags='theline', fill='black')
+        self.canvas.create_line(self.point_list, tags='theline', width=80)
+        self.canvas.create_oval(event.x-40, event.y-40, event.x+40, event.y+40, tags='theline', fill='black')
 
-        self.draw.line(self.point_list, 'black', width=20)
+        self.draw.line(self.point_list, 'black', width=80)
 
         self.point_list = self.point_list[-2:]
 
@@ -59,36 +60,62 @@ class TKApp:
 
     def save(self):
         im = self.trim(self.image1)
-        #dont do resizing here like this. it cuts out the black entirely and messed up the answer
-        resized = im.resize([8,8])
-        resized.save('C:/test/tsst.jpg')
-        print(self.convert_img_array(resized))
-        print('test')
-
-        out = self.network.getoutput(self.convert_img_array(resized))
+        im.save('tsst.jpg')
+        out = self.network.getoutput(self.convert_img_array(im))
         answer = self.convert_to_answer(out)
         print(answer)
 
     def trim(self, image):
-        bg = Image.new(image.mode, image.size, image.getpixel((0, 0)))
-        diff = ImageChops.difference(image, bg)
-        diff = ImageChops.add(diff, diff, 2.0, -100)
-        bbox = diff.getbbox()
-        if bbox:
-            return self.image1.crop(bbox)
+        #bg = Image.new(image.mode, image.size, image.getpixel((0, 0)))
+        #diff = ImageChops.difference(image, bg)
+        #diff = ImageChops.add(diff, diff, 2.0, -100)
+        #bbox = diff.getbbox()
+
+        #image = self.image1.crop(bbox)
+
+        size = (32, 32)
+        #image.thumbnail(size)
+        #background = Image.new('RGBA', size, (255, 255, 255, 0))
+        #background.paste(
+        #    image,
+        #    ((size[0] - image.size[0]) // 2, (size[1] - image.size[1]) // 2))
+
+        thumb = ImageOps.fit(image,size,Image.ANTIALIAS)
+        return thumb
 
     def convert_img_array(self, img):
         # work needs to be done here to compress images correctly.
         # needs to take the image and shrink it to an array using the original method
         # (take groups of pixels and compress into squares)
-        arr = img.getdata()
-        new_arr = []
-        for i in arr:
-            if sum(i) == 0:
-                new_arr.append(15)
-            else:
-                new_arr.append(0)
-        return new_arr
+
+        #put all pixels into a dictionary that organizes everything into boxes
+        box_dict = {}
+        for i in range(8):
+            for j in range(8):
+                box_dict[(i,j)] = []
+
+        for value, i in zip(img.getdata(), range(len(img.getdata()))):
+            box_row = i // 32 // 4
+            box_column = i % 32 // 4
+            box_dict[(box_row, box_column)].append(value)
+
+        out_array = []
+        box_sum_list = []
+        for i in range(8):
+            for j in range(8):
+                box_sum = 0
+                for item in box_dict[i, j]:
+                    box_sum += sum(item[:-1])
+                box_sum_list.append(box_sum)
+
+        for val in box_sum_list:
+            out_array.append(val//510)
+
+        #invert numbers. black needs to be max val, not white
+        for i in range(len(out_array)):
+            out_array[i] = -(out_array[i] - 8) + 8
+
+        return out_array
 
     def convert_to_answer(self, output_list):
         most_confident = -1
