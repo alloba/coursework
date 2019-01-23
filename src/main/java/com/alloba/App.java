781901d7bfed47bda6989b49159abf5646549e8a
Alex <alexlbates@gmail.com>
2019-01-23 12:54:18 -0500

    initial commit

---
+++ src/main/java/com/alloba/App.java
@@ -0,0 +1,13 @@
+package com.alloba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
